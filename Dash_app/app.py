eb2d8e8e52e619f3664e7e5c307b5c1b010e9267
a0kim <003julie@gmail.com>
2022-06-05 20:50:50 +0900

    네브바(헤더)생성, 배경색, 지도 삽입

--- Dash_app/app.py
+++ Dash_app/app.py
@@ -1,18 +1,22 @@
 from flask import Flask
-from dash import Dash, dcc, html
+from dash import Input, Output, State, Dash, dcc, html
 import dash_bootstrap_components as dbc
-
-
+import dash_leaflet as dl
 import pandas as pd
 import plotly.express as px
 
+
+
+# map_osm = folium.Map(location=[35.166804, 129.083479], zoom_start=12)
+
 server = Flask(__name__)
 app = Dash(__name__,
+            prevent_initial_callbacks=True,
            meta_tags=[{'name': 'viewport',
                        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}],
            external_stylesheets=[dbc.themes.BOOTSTRAP],
            server=server
-)
+           )
 
 # @app.route('/')
 # def hello_world():  # put application's code here
@@ -27,12 +31,22 @@ df = pd.DataFrame({
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 fig_1 = px.pie(df, values='Amount', names='Fruit')
 
-app.layout = html.Div(children=[
-    html.H1(children='Dash에서 h1부분'),
+fig.update_layout({
+    'paper_bgcolor': '#E9EEF6',
+})
+fig_1.update_layout({
+    'paper_bgcolor': '#E9EEF6',
+})
 
-    html.Div(children='''
-        걍 잡것 씨부리기.
-    '''),
+navbar = dbc.NavbarSimple(
+    brand="GVCS",
+    brand_style={"color": "black"},
+    brand_href="#",
+    color="beige",
+    dark=True,
+)
+
+chart = html.Div(
     dbc.Row([
         dbc.Col([
             dbc.Row([
@@ -40,14 +54,15 @@ app.layout = html.Div(children=[
                     dcc.Graph(
                         className="image",
                         id='1',
-                        figure=fig_1
+                        figure=fig_1,
                     ),
                 ], xs=6, sm=6, md=6, lg=12, xl=12),
                 dbc.Col([
                     dcc.Graph(
                         className="image",
                         id='2',
-                        figure=fig_1
+                        figure=fig_1,
+                        style={'background-color': '#E9EEF6'}
                     ),
                 ], xs=6, sm=6, md=6, lg=12, xl=12)
             ]),
@@ -142,8 +157,42 @@ app.layout = html.Div(children=[
                 ], xs=12, sm=12, md=12, lg=12, xl=12)
             ])
         ], xs=12, sm=12, md=12, lg=2, xl=2),
+    ], id='row'),
+)
+
+# map=
+
+app.layout = dbc.Container(className='main', children=[
+    dbc.Row([
+        dbc.Col(navbar),
     ]),
+    dbc.Row([
+        dbc.Col(chart),
+    ]),
+    dbc.Row([
+        dbc.Col(dl.Map([dl.TileLayer(), dl.LayerGroup(id="layer")],
+           id="map", style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),),
+    ])
+
 ])
+
+
+@app.callback(
+    Output("layer", "children"), [Input("map", "click_lat_lng")],)
+def map_click(click_lat_lng):
+    return [dl.Marker(position=click_lat_lng, children=dl.Tooltip("({:.3f}, {:.3f})".format(*click_lat_lng)))]
+
+
+# @app.callback(
+#     Output("navbar-collapse", "is_open"),
+#     # [Input("navbar-toggler", "n_clicks")],
+#     [State("navbar-collapse", "is_open")],
+# )
+# def toggle_navbar_collapse(n, is_open):
+#     if n:
+#         return not is_open
+#     return is_open
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
