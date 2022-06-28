21c677ff3bc8cc3c9295582245a2429c1ea5fe51
jyc4324 <101799316+jyc4324@users.noreply.github.com>
2022-06-29 04:20:06 +0900

    기기 적응형 차트 수정, 지도 수정

--- flask_sever/app.py
+++ flask_sever/app.py
@@ -13,23 +13,25 @@ app = Dash(__name__,
                        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
            )
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    "경제적": ["True", "False"],
+    "확률": [80, 20],
+    "요소": ["True", "False"]
+})
+standard_df = pd.DataFrame({
+    "기준": ["환경적", "경제적", "기술적", "사회적"],
+    "Amount": [4, 1, 2, 3]
 })
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-fig_1 = px.pie(df, values='Amount', names='Fruit')
+fig = px.bar(df, x="경제적", y="확률", color="요소")
+fig_1 = px.pie(standard_df, values='Amount', names='기준')
+
 fig.update_layout({
     'paper_bgcolor': '#E9EEF6',
-})
+}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15)
 fig_1.update_layout({
     'paper_bgcolor': '#E9EEF6',
-})
+}, margin_l=0, margin_r=0, margin_b=20, margin_t=40, legend_y=1.61, legend_x=0.25, legend_orientation="h")
 
 app.layout = html.Div(className='main', children=[
     html.H1(children='Dash에서 h1부분'),
@@ -43,18 +45,18 @@ app.layout = html.Div(className='main', children=[
             dbc.Row([
                 dbc.Col([
                     dcc.Graph(
-                        className="image",
+                        className="standard",
                         id='1',
                         figure=fig_1,
                     ),
-                ], xs=6, sm=6, md=6, lg=12, xl=12),
+                ], xs=6, sm=6, md=6, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
                     dcc.Graph(
-                        className="image",
+                        className="standard",
                         id='2',
                         figure=fig_1
                     ),
-                ], xs=6, sm=6, md=6, lg=12, xl=12)
+                ], xs=6, sm=6, md=6, lg=12, xl=12, style={'padding': '12px'})
             ]),
         ], xs=12, sm=12, md=12, lg=2, xl=2),
         dbc.Col([
@@ -65,16 +67,17 @@ app.layout = html.Div(className='main', children=[
                         id='3',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12),
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
                     dcc.Graph(
                         className="image",
                         id='4',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12)
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
         ], xs=12, sm=12, md=12, lg=2, xl=2),
+
         dbc.Col([
             dbc.Row([
                 dbc.Col([
@@ -83,14 +86,14 @@ app.layout = html.Div(className='main', children=[
                         id='5',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12),
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
                     dcc.Graph(
                         className="image",
                         id='6',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12)
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
         ], xs=12, sm=12, md=12, lg=2, xl=2),
         dbc.Col([
@@ -101,14 +104,14 @@ app.layout = html.Div(className='main', children=[
                         id='7',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12),
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
                     dcc.Graph(
                         className="image",
                         id='8',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12)
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
         ], xs=12, sm=12, md=12, lg=2, xl=2),
         dbc.Col([
@@ -119,14 +122,14 @@ app.layout = html.Div(className='main', children=[
                         id='9',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12),
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
                     dcc.Graph(
                         className="image",
                         id='10',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12)
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
         ], xs=12, sm=12, md=12, lg=2, xl=2),
         dbc.Col([
@@ -137,22 +140,27 @@ app.layout = html.Div(className='main', children=[
                         id='11',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12),
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
                     dcc.Graph(
                         className="image",
                         id='12',
                         figure=fig
                     ),
-                ], xs=12, sm=12, md=12, lg=12, xl=12)
+                ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
         ], xs=12, sm=12, md=12, lg=2, xl=2),
     ], className="chart"),
     html.Br(),
-    html.Div(children=[
-        dl.Map(dl.TileLayer(), className='map')
-    ], className='map'),
-    html.Br()
+    # html.Div(children=[
+    #     dl.Map(dl.TileLayer(), className='map')
+    # ], className='map'),
+
+    html.Iframe(
+        src="assets/route_graph.html",
+        style={"height": "500px", "width": "95%"},
+    ),
+    html.P(),
 ])
 
 if __name__ == '__main__':
