00d7e94ceccd2b83e9a94cf2f7c2654a516e2b38
a0kim <003julie@gmail.com>
2022-06-30 03:55:28 +0900

    대기질 연결 베이지안네트워크 Div 및 layout 구분

--- Dash_app/app.py
+++ Dash_app/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
+import air_quality
 
 server = Flask(__name__)
 app = Dash(__name__,
@@ -12,6 +13,23 @@ app = Dash(__name__,
            meta_tags=[{'name': 'viewport',
                        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
            )
+
+ozone_file_path = "assets/오존_월별_도시별_대기오염도.csv"
+df_ozone = pd.read_csv(ozone_file_path, encoding='cp949')
+ozen_col =  df_ozone.iloc[:,2:-1].columns.tolist()
+df_ozone.drop(columns=ozen_col, inplace=True)
+
+# 해당 컬럼 float 타입으로 변경
+air_quality.advanced_replace(df_ozone, df_ozone.iloc[:,2:].columns.tolist(), '-', r'[^0-9.0-9]')
+df_ozone['2021.07'] = df_ozone['2021.07'].astype(float)
+
+
+air_quality.show_norm(df_ozone.iloc[:,2].mean(), df_ozone.iloc[:,2].std(), df_ozone.iloc[:,2].min(), df_ozone.iloc[:,2].max())
+### 부산광역시 오존의 누적확률 구하기
+
+
+busan_oz = df_ozone[df_ozone['구분(2)']=='부산광역시'].loc[2,'2021.07']
+fig_ozone = air_quality.cal_norm(df_ozone.iloc[:,2].mean(),df_ozone.iloc[:,2].std(),df_ozone.iloc[:,2].min(),df_ozone.iloc[:,2].max(),busan_oz)
 
 df_economy = pd.DataFrame({
     "경제적": ["True", "False"],
@@ -57,17 +75,21 @@ fig_2 = px.pie(hydro_standard_df, values='Amount', names='기준')
 
 fig1.update_layout({
     'paper_bgcolor': '#E9EEF6',
-}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,)
+}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15, legend_title_text="",)
 fig2.update_layout({
     'paper_bgcolor': '#E9EEF6',
-}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,)
+}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,legend_title_text="")
 fig3.update_layout({
     'paper_bgcolor': '#E9EEF6',
-}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,)
+}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,legend_title_text="")
 fig4.update_layout({
     'paper_bgcolor': '#E9EEF6',
-}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,)
+}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15,legend_title_text="")
 
+
+fig_ozone.update_layout({
+    'paper_bgcolor': '#E9EEF6',
+}, margin_l=10, margin_r=10, legend_y=1.5, legend_x=0.15, title_text='대기질',title_y=0.1,title_x=0.5)
 
 fig_1.update_layout({
     'paper_bgcolor': '#E9EEF6',
@@ -94,7 +116,7 @@ navbar = dbc.Navbar(
                 #     href="#",
                 #     className="logoImg",
                 # )
-                dbc.Button("전체 확률 네트워크 보기 ->", outline=True, color="secondary", className="me-1",)
+                dbc.Button("전체 확률 네트워크 보기 →", outline=True, color="secondary", className="me-1",)
             )
         ]
     )
@@ -120,7 +142,7 @@ chart = html.Div(
                     ),
                 ], xs=6, sm=6, md=6, lg=12, xl=12, style={'padding': '12px'})
             ]),
-        ], xs=12, sm=12, md=12, lg=4, xl=2.4, className="pie_chart"),
+        ], xs=12, sm=12, md=12, lg=4, xl=4, className="pie_chart"),
 
         dbc.Col([
             dbc.Row([
@@ -139,7 +161,7 @@ chart = html.Div(
                     ),
                 ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
-        ], xs=12, sm=12, md=12, lg=2, xl=2.4),
+        ], xs=12, sm=12, md=12, lg=2, xl=2),
         dbc.Col([
             dbc.Row([
                 dbc.Col([
@@ -157,7 +179,7 @@ chart = html.Div(
                     ),
                 ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
-        ], xs=12, sm=12, md=12, lg=2, xl=2.4, className="chart_bar_1"),
+        ], xs=12, sm=12, md=12, lg=2, xl=2, className="chart_bar_1"),
 
         dbc.Col([
             dbc.Row([
@@ -165,7 +187,7 @@ chart = html.Div(
                     dcc.Graph(
                         className="image",
                         id='7',
-                        figure=fig1
+                        figure=fig_ozone
                     ),
                 ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'}),
                 dbc.Col([
@@ -176,7 +198,7 @@ chart = html.Div(
                     ),
                 ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
-        ], xs=12, sm=12, md=12, lg=2, xl=2.4),
+        ], xs=12, sm=12, md=12, lg=2, xl=2),
         dbc.Col([
             dbc.Row([
                 dbc.Col([
@@ -194,11 +216,9 @@ chart = html.Div(
                     ),
                 ], xs=12, sm=12, md=12, lg=12, xl=12, style={'padding': '12px'})
             ])
-        ], xs=12, sm=12, md=12, lg=2, xl=2.4),
+        ], xs=12, sm=12, md=12, lg=2, xl=2),
     ], className="chart")
 )
-
-
 
 app.layout = html.Div(className='main', children=[
     navbar,
